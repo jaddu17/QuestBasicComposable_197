2f69394881fe5ec13071cc51fc02bea1e82c3fdf
jaddu17 <shafajaddu@gmail.com>
2025-10-17 11:09:21 +0700

    menambahkan Box

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -128,6 +128,12 @@ fun TataletakBoxColomnRow(modifier: Modifier){
             }
         }
         Spacer(modifier = Modifier.height(height = 10.dp))
-
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        )
     }
 }
