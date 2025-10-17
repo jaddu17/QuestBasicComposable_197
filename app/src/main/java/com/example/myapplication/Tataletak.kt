655efa779394b0aeced01e6e401b29f3dbf6a5c4
jaddu17 <shafajaddu@gmail.com>
2025-10-17 11:00:41 +0700

    menambahkan tataletakcolumnrow baris 1

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -44,3 +44,15 @@ fun TataletakRow(modifier: Modifier){
     }
 }
 
+@Composable
+fun TataletakColumnRow(modifier: Modifier){
+    Column(){
+        //Baris1
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+    }
+}
+
