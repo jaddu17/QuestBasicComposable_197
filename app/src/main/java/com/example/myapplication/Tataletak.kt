ce3f284045da7ff589599d2c28e828e617bd2911
jaddu17 <shafajaddu@gmail.com>
2025-10-17 11:03:38 +0700

    menambahkan tataletakRowColumn Kolom 2

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -88,7 +88,12 @@ fun TataletakRowColumn(modifier: Modifier){
             Text(text = "Komponen2Kolom1")
             Text(text = "Komponen3Kolom1")
         }
-
+        //Kolom2
+        Column () {
+            Text(text = "Komponen1Kolom1")
+            Text(text = "Komponen2Kolom1")
+            Text(text = "Komponen3Kolom1")
+        }
     }
 }
 
