50a7c3da3e279f4ae8e46b261c7f4abf44eb7eb8
jaddu17 <shafajaddu@gmail.com>
2025-10-17 11:02:11 +0700

    menambahkan tataletakbox

--- app/src/main/java/com/example/myapplication/Tataletak.kt
+++ app/src/main/java/com/example/myapplication/Tataletak.kt
@@ -64,3 +64,18 @@ fun TataletakColumnRow(modifier: Modifier){
     }
 }
 
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ){
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
